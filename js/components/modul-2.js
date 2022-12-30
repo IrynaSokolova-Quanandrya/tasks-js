18b98778cf1f12f62d067c02d983fbcc494e0b26
Iryna <irasokolova25@ukr.net>
2022-12-30 16:48:56 +0200

    add tasks for test

--- js/components/modul-2.js
+++ js/components/modul-2.js
@@ -71,33 +71,35 @@ isObjectEmpty({name: 'user, age: {21}}) // false
 
 7. Данно масив з обєктами. Виконати всі поставлені задачі використовуючи функціональні методи масивів
 
-7.1 отримати масив вчених що народилися в 19 ст
-7.2 знайти суму років скільки прожили всі вченні
-7.3 Відсортувати вчених по алфавіту
-7.4 Відсортувати вчених по кількості прожитих років
-7.5 Видалити з масива вчених що народилися в 15 або 16 або 17 столітті
-7.6 Знайти вченого який народився найпізніше.
-7.7 Знайти рік народження Albert Einstein
-7.8 знайти вчених прізвище яких починається на літеру С
-7.9 Видалити з масива всіх вчених імя яких починається на A
-7.10 Знайти вченого який прожив найбільше і вченого який прожив найменьше
-7.11 Знайти вчених в яких співпадають перші літери імені і прізвища
-7.12 Дізнатися чи всі вченні працювали в 19 столітті
+    7.1 отримати масив вчених що народилися в 19 ст
+    7.2 знайти суму років скільки прожили всі вченні
+    7.3 Відсортувати вчених по алфавіту
+    7.4 Відсортувати вчених по кількості прожитих років
+    7.5 Видалити з масива вчених що народилися в 15 або 16 або 17 столітті
+    7.6 Знайти вченого який народився найпізніше.
+    7.7 Знайти рік народження Albert Einstein
+    7.8 знайти вчених прізвище яких починається на літеру С
+    7.9 Видалити з масива всіх вчених імя яких починається на A
+    7.10 Знайти вченого який прожив найбільше і вченого який прожив найменьше
+    7.11 Знайти вчених в яких співпадають перші літери імені і прізвища
+    7.12 Дізнатися чи всі вченні працювали в 19 столітті
 
-const arr = [
-{ name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
-{ name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
-{ name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
-{ name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
-{ name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 5 },
-{ name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 6 },
-{ name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 7 },
-{ name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 8 },
-{ name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 9 },
-{ name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 10 },
-{ name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 11 },
-{ name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 12 }
-]
+    const arr = [
+    { name: "Albert", surname: "Einstein", born: 1879, dead: 1955, id: 1 },
+    { name: "Isaac", surname: "Newton", born: 1643, dead: 1727, id: 2 },
+    { name: "Galileo", surname: "Galilei", born: 1564, dead: 1642, id: 3 },
+    { name: "Marie", surname: "Curie", born: 1867, dead: 1934, id: 4 },
+    { name: "Johannes", surname: "Kepler", born: 1571, dead: 1630, id: 5 },
+    { name: "Nicolaus", surname: "Copernicus", born: 1473, dead: 1543, id: 6 },
+    { name: "Max", surname: "Planck", born: 1858, dead: 1947, id: 7 },
+    { name: "Katherine", surname: "Blodgett", born: 1898, dead: 1979, id: 8 },
+    { name: "Ada", surname: "Lovelace", born: 1815, dead: 1852, id: 9 },
+    { name: "Sarah E.", surname: "Goode", born: 1855, dead: 1905, id: 10 },
+    { name: "Lise", surname: "Meitner", born: 1878, dead: 1968, id: 11 },
+    { name: "Hanna", surname: "Hammarström", born: 1829, dead: 1909, id: 12 }
+    ]
+    .map(el)
+    .sort((a, b)=>a.surname - b.surname)
 
 8. Створити кнопку та інпут. Реалізувати відтворення рандомного числа в інпуті при натисканні на кнопку.
 
